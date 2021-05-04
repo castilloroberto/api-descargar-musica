5874390fec8dd04acfd253895dca08d1d12a0fc2
robertocastillo <robertocastillo945@gmail.com>
2021-05-04 15:08:14 -0600

    removi paquetes innesearios

--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const favicon = require('serve-favicon')
-const path = require('path')
-const morgan = require('morgan')
 const app = express()
 
 //ajustes y archivos estaticos
-app.set('port',3000)
-app.use(express.static(path.join(__dirname,'public')))
-app.use(favicon(path.join(__dirname,'public','favicon.ico')))
+app.set('port',process.env.PORT || 3000)
 
 //middlewares
 app.use(cors())
-app.use(morgan('dev'))
 app.use(express.json())
 
 //rutas

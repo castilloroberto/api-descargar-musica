9d908b20f475a45ed0f50a96e116810b52f30ed4
robert-virtual <robertocastill63@gmail.com>
2021-12-20 10:14:39 -0600

    version 2

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 //ajustes y archivos estaticos
-app.set('port',process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 
 //middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //rutas
-app.use(require('./routes/download'))
+app.use(require("./routes/download"));
+app.use("api/v2/", require("./routes/videos"));
 
-app.listen(app.get('port'),()=>{
-    console.log(`Server running on port ${app.get('port')}`);
-})
-
+app.listen(app.get("port"), () => {
+  console.log(`Server running on port ${app.get("port")}`);
+});
